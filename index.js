911f47d93f22ecfa65971feddc7dfc5147f0da6f
Julia Lidstrom <juleslidstrom@gmail.com>
2017-10-04 01:20:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string){
+  return string.toUpperCase()
+};
+
+function whisper(string){
+  return string.toLowerCase()
+};
+
+function logShout(string){
+  console.log('HELLO');
+};
+
+function logWhisper(string){
+  console.log('hello');
+};
+
+
+function sayHiToGrandma(whisper){
+  return ('I can\'t hear you!')
+}
+
+function sayHiToGrandma(shout){
+  return('YES INDEED!')
+}
+
+
